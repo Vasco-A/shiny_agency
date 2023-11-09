36373d7cde86dad395a2f0f6bb6d94661daa1ec2
Vasco_A <v.agnello@deuse.be>
2023-11-09 10:28:09 +0100

    usable site + context + result page retreiving data

---
+++ src/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Survey from './pages/Survey'
+import Results from './pages/Results'
+import Freelances from './pages/Freelances'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Error from './components/Error'
+
+import { ThemeProvider } from './utils/context'
+import { SurveyProvider } from './utils/context'
+import GlobalStyle from './utils/style/GlobalStyle'
+ 
+ReactDOM.render(
+    <React.StrictMode>
+        <Router>
+			<ThemeProvider>
+			<SurveyProvider>
+				<GlobalStyle />	
+				<Header />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/survey/:questionNumber" element={<Survey />} />
+					<Route path="/results" element={<Results />} />
+					<Route path="/freelances" element={<Freelances />} />
+					<Route path="*" element={<Error />} />
+				</Routes>
+				<Footer />
+			</SurveyProvider>
+			</ThemeProvider>
+        </Router>
+    </React.StrictMode>,
+document.getElementById('root')
+)
